f506d9ae6a60f83fa143e5f538a845e7488108de
EgleLtv <83534768+EgleLtv@users.noreply.github.com>
2021-09-29 09:29:34 +0100

    draft v2.0

--- project-draft.py
+++ project-draft.py
@@ -1,15 +1,12 @@
-import requests
 import os
-import sys
+import requests
 
 # clear text file for ease of debugging each time the program is started
 file = open('recipetext.txt', 'r+')
 file.truncate(0)
 
-print('Welcome to RecipSearch!\n'
-      'We\'ll choose a couple of recipes based on an ingredient of your choice'
-      'and your dietary preferences')
-
+# dictionary to save calories data for review
+dict_calories = {}
 
 # Edamam API open and return data
 def search_recipe(ingredient):
@@ -18,101 +15,149 @@ def search_recipe(ingredient):
     results = requests.get(
         'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key))
     data = results.json()
+
     return data['hits']
 
 
-# list to hold ingredients
-recipe_list = []
+def print_recipe(recipe_num, recipe, labels, recipe_ing):
+    print('\nThe recipe number {}\'s label is {}.\n'
+          'Total calories are {}.\n'
+          'You can access it here: {}'.format(recipe_num, recipe['label'], int(recipe['calories']),
+                                              recipe['url']))
+
+    print('Recipe is suitable for people following these diets: {}'.format(', '.join(labels)))
+
+    # print all ingredients of the recipe and save to a list
+    print('The recipe calls for these ingredients:')
+
+    ing_values = recipe['ingredients']
+
+    for ing in ing_values:
+        print(ing['text'])
+        recipe_ing.append(ing['text'])
+
+    return None
+
+
+def save_recipe(recipe, recipe_ing):
+    save_recipes = input('Do you want to save this recipe into a file? Y/N\n').lower()
+
+    if save_recipes == 'y':
+
+        # save recipe info for recipe calories review
+        add_dict_value = {recipe['label']: int(recipe['calories'])}
+        dict_calories.update(add_dict_value)
+
+        # FOR DEBUGGING PURPOSES ONLY!
+        print(add_dict_value)
+        print(dict_calories)
+
+        # append text file to add recipe info
+        with open('recipetext.txt', 'a') as recipe_file:
+
+            recipe_file.write(recipe['label'] + '\n')
+            recipe_file.write(recipe['url'] + '\n')
+
+            for element in recipe_ing:
+                recipe_file.write(element + '\n')
+
+            recipe_file.write('\n')
+
+        # open appended text file
+        os.startfile("recipetext.txt")
+
+    return None
+
+
+def recipe_review():
+    s = sorted(dict_calories.items(), key=lambda x: x[1], reverse=True)
+    print('\nYou have saved the recipes below.\n'
+          'The recipes are listed from highest total calories to lowest in a descending order.')
+    for k, v in s:
+        print('Recipe name: {}, total calories: {}'.format(k, v))
+
+    min_value = min(dict_calories.values())
+    min_keys = min(dict_calories, key=dict_calories.get)
+    print('The recipe {} has lowest calories. Calories are {}'.format(min_keys, min_value))
+    return None
 
 
 # start search by entering ingredient
 def run():
-    results = search_recipe(input('Please enter an ingredient'))
-    # diet labels input
-    diet_choice = ''
-    diet_label = input('Do you have any dietary preferences? \n'
-                       'VG-vegetarian\n'
-                       'V-vegan\n'
+    results = search_recipe(input('What ingredient do you want a recipe for?\n'))
+
+    diet_label = input('Do you have any dietary preferences?\n'
+                       'V-vegetarian\n'
+                       'VG-vegan\n'
                        'PC-pescatarian\n'
                        'GF-gluten-free\n'
-                       'N-no preferences').lower()
-    if diet_label == 'vg':
+                       'N-no preferences\n').lower()
+
+    if diet_label == 'v':
         diet_choice = 'Vegetarian'
-    elif diet_label == 'v':
+
+    elif diet_label == 'vg':
         diet_choice = 'Vegan'
+
     elif diet_label == 'pc':
         diet_choice = 'Pescatarian'
+
     elif diet_label == 'gf':
         diet_choice = 'Gluten-Free'
+
     else:
         diet_label = 'n'
-        print('You did not choose any diet preferences.')
+        diet_choice = ''
+        print('You did not choose any diet preferences\n')
 
-    recipe_num = 1
-    # initialise for loop to go through recipes
+    # list to hold ingredients
+    recipe_ing = []
+
+    recipe_num = 0
+
     for result in results:
         recipe = result['recipe']
         labels = recipe['healthLabels']
-        check = False
 
-        # check if diet label is mentioned
-        for item in labels:
-            if item == diet_choice:
-                check = True
+        # print recipe if dietary preferance matches or if no dietary preference selected
+        if (diet_choice in labels) or (diet_label == 'n'):
 
-        # print recipe if no diet selected or if diet matches
-        if check or diet_label == 'n':
-            print('The recipe number {}\'s label is {}.\n'
-                  'Total calories are {}.\n'
-                  'You can access it here: {}'.format(recipe_num, recipe['label'], int(recipe['calories']),
-                                                      recipe['url']))
-            print('Recipe is suitable for people following these diets: {}'.format(', '.join(labels)))
-            # print all ingredients of the recipe and save to a list
-            print('The recipe calls for these ingredients:')
-            ing_values = recipe['ingredients']
-            for ing in ing_values:
-                print(ing['text'])
-                recipe_list.append(ing['text'])
             recipe_num += 1
 
-            # save recipe to file, including ingredient list
-            save_recipes = input('Do you want to save this recipe into a file? Y/N').lower()
-            if save_recipes == 'y':
-                with open('recipetext.txt', 'a') as recipe_file:
-                    recipe_file.write(recipe['label'] + '\n')
-                    recipe_file.write('Total calories: ' + str(int(recipe['calories'])) + '\n')
-                    recipe_file.write(recipe['url'] + '\n')
-                    for element in recipe_list:
-                        recipe_file.write(element + '\n')
-                    recipe_file.write('\n')
+            # print recipe details to terminal
+            print_recipe(recipe_num, recipe, labels, recipe_ing)
 
-            # ask if continue or not
-            do_continue = input('Do you want to see another recipe? Y/N').lower()
+            # save recipe to a file, including the ingredient list
+            save_recipe(recipe, recipe_ing)
+
+            # ask if users want to continue or not
+            do_continue = input('Do you want to see another recipe? Y/N \n').lower()
+
             if do_continue == 'n':
-                if os.stat('recipetext.txt').st_size == 0:
-                    print('Sorry you did not like the recipes.')
-                else:
-                    print('Saved recipes can be found in a text file named recipetext.txt :)')
-                sys.exit('Bye, thank you for using RecipSearch!')
-
-            # clear list to start next recipe
-            recipe_list.clear()
+                break
 
     # ask if want to try again
     if os.stat('recipetext.txt').st_size == 0:
-        start_over = input('\nNo more recipes to show, sorry you didn\'t see anything you liked.\n'
-                           'Would you like to try again with a different ingredient? Y/N').lower()
-        if start_over == 'y':
-            run()
-            print('Bye, thank you for using RecipSearch!')
+
+        start_over = input('Sorry you didn\'t see anything you liked!\n'
+                           '\nWould you like to find recipes for a different ingredient instead? Y/N\n').lower()
+
     else:
-        print('\nNo more recipes to show, thank you for using RecipSearch! :)\n'
-              'Saved recipes can be found in a text file named recipetext.txt')
-        start_over = input('Would you like to try again with a different ingredient? Y/N').lower()
-        if start_over == 'y':
-            run()
-            print('Bye, thank you for using RecipSearch!')
+        start_over = input('No more recipes to show!\n'
+                           '\nSaved recipes can be found in a text file named recipetext.txt\n'
+                           '\nWould you like to find recipes for another ingredient? Y/N\n').lower()
+
+    if start_over == 'y':
+        run()
+    # ask if want to review calories data for recipes that have been saved to a file
+    else:
+        do_review_recipes = input('Would you like to review calorific information of the recipes saved? Y/N').lower()
+        if do_review_recipes == 'y':
+            recipe_review()
+
+    return None
 
 
 # run program
 run()
+print('\nBye, thank you for using RecipeSearch!')
